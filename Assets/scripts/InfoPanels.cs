a1af017b1d4d2b802b65d1420d9e9c44716a0cd2
ManjitBedi <work@manjitbedi.com>
2017-03-20 15:50:22 -0700

    initial commit

---
+++ Assets/scripts/InfoPanels.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPanels : MonoBehaviour {
+
+
+  	public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ShowPanels () {
+
+		if (cam == null) {
+		cam = Camera.main;
+		}
+
+		if (cam != null) {
+			// Tie this to the camera, and do not keep the local orientation.
+			transform.SetParent(cam.GetComponent<Transform>(), true);
+		}
+	}
+
+	public void HidePanels () {
+
+
+	}
+}
---
+++ Assets/scripts/station.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class station : MonoBehaviour {
+
+	public float speed = 10f;
+
+	public bool playing = false;
+	public bool played = false;
+
+	public bool prompted = false; 
+
+	public GameObject mesh;
+
+	public GameObject stationContent;
+
+	public GvrAudioSource promptAudio;
+	public GvrAudioSource contentAudio;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    
+    void Update ()
+    {
+        mesh.transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+
+
+
+
+	public void OnEnterGaze () {
+
+		if (!prompted) {
+			prompted = true;
+			if (promptAudio != null) {
+				promptAudio.Play();
+			}
+		}
+	}
+
+	public void OnExitGaze () {
+
+	}
+
+	public void OnClick () {
+
+		if (promptAudio != null) {
+			if (promptAudio.isPlaying) {
+				promptAudio.Stop();
+			}
+		}
+
+		if (!playing) {
+			playing = true;
+			stationContent.SetActive (true);
+			if (contentAudio != null) {
+				contentAudio.Play();
+			}
+		} else {
+			playing = false;
+				stationContent.SetActive (false);
+			if (contentAudio != null) {
+				contentAudio.Stop();
+			}
+		}
+	}
+}
